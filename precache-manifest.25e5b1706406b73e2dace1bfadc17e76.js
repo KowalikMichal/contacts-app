2feb88b8db5a907719ab2afbda3fd4e0e1dec6d5
Michał Kowalik <michal.kowalik.94@gmail.com>
2019-02-07 23:26:22 +0100

    Updates

---
+++ precache-manifest.25e5b1706406b73e2dace1bfadc17e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01d0a9f185abe75b2276",
+    "url": "/static/js/main.01d0a9f1.chunk.js"
+  },
+  {
+    "revision": "dffdf0f8ef057a6ff65a",
+    "url": "/static/js/1.dffdf0f8.chunk.js"
+  },
+  {
+    "revision": "01d0a9f185abe75b2276",
+    "url": "/static/css/main.e680af8c.chunk.css"
+  },
+  {
+    "revision": "4bdf3bd2bc40f1503c08351e5e5cc69e",
+    "url": "/index.html"
+  }
+];
